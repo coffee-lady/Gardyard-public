0d405d397783c8c66338cd9fc18f76fef9c34cbd
Sima Leitch <sima.leitch@gmail.com>
2020-10-30 01:08:18 +0300

    p

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = require('./config/express');
 require('./config/mongoose');
 
 if (!module.parent) {
-    app.listen(config.port, '0.0.0.0', () => {
+    app.listen(config.port || 5000, '0.0.0.0', () => {
         console.info(`server started on port ${config.port} (${config.env})`);
     });
 }
